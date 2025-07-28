36eafacdb5be0b68f6fda963b4e4c98a86b4a95f
Park-Mu-Seong <63047990+CodeVac513@users.noreply.github.com>
2025-07-28 16:39:26 +0900

    [Bronze V] Title: 새로운 시작, Time: 96 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw.write("파이팅!!");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
