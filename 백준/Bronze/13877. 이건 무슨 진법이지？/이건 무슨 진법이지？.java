9ef25cec6ab0c14ec58a3fe3b14ffd77820e5466
Park-Mu-Seong <63047990+CodeVac513@users.noreply.github.com>
2025-09-03 23:17:45 +0900

    [Bronze II] Title: 이건 무슨 진법이지?, Time: 140 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Bronze/13877. 이건 무슨 진법이지？/이건 무슨 진법이지？.java
+++ 백준/Bronze/13877. 이건 무슨 진법이지？/이건 무슨 진법이지？.java
@@ -1,35 +1,55 @@
 import java.util.*;
-public class Main {
+import java.lang.*;
+import java.io.*;
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int T = scan.nextInt();
-		for (int i = 0; i < T; i++) {
-			System.out.print(scan.next() + " ");
-			
-			String D = scan.next();
-			boolean o = true;
-			int oct = 0;
-			for (int j = 0; j < D.length(); j++) {
-				int ith = Integer.parseInt(D.substring(j, j+1));
-				oct += ith * Math.pow(8, D.length()-1-j);
-				if (ith >= 8) {
-					o = false;
-					break;
-				}
-			}
-			System.out.print(o ? oct + " " : 0 + " ");
-			System.out.print(Integer.parseInt(D) + " ");
-			int hex = 0;
-			for (int j = 0; j < D.length(); j++) {
-				hex += Integer.parseInt(D.substring(j, j+1)) 
-						* Math.pow(16, D.length()-1-j);
-			}
-			System.out.println(hex);
-		}
-		
-		scan.close();
-	}
+public class Main {
+    // 13877번
+    // 구현?
+    // 숫자 하나를 입력 받으면 그 숫자를 8진수와 16진수로도 표현해야 함.
+    // 오답 노트)
+    // 숫자가 각각 8진수, 16진수로 나타내진 수라고 가정해야 함. 십진수 num을 다른 진법으로 나타내는 문제가 아님.
+    // 오답 노트2)
+    // 자릿수에 8이 오는 경우도 제외해야 함.
+    // 오답 노트3)
+    // 0으로 시작하는 경우를 지우기 위해 num을 integer로 변환했다가 다시 String으로 변환
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= testCase; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int index = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            bw.write(index + " " + octalToDecimal(String.valueOf(num)) + " " + num + " " + hexadecimalToDecimal(String.valueOf(num)));
+            bw.write("\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static int octalToDecimal(String num) {
+        int sum = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int currentNum = Character.getNumericValue(num.charAt(num.length() - 1 - i));
+            if (currentNum >= 8) {
+                return 0;
+            }
+            sum += (currentNum * (int) Math.pow(8, i));
+        }
+
+        return sum;
+    }
+
+    public static int hexadecimalToDecimal(String num) {
+        int sum = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int currentNum = Character.getNumericValue(num.charAt(num.length() - 1 - i));
+            sum += (int) (currentNum * Math.pow(16, i));
+        }
+        return sum;
+    }
 
 }
