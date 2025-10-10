2af525f6796d64801fb35aea6973ea97cce4b675
Park-Mu-Seong <63047990+CodeVac513@users.noreply.github.com>
2025-10-10 14:05:51 +0900

    [level 2] Title: 문자열 압축, Time: 6.06 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,65 +1,72 @@
 import java.util.*;
-import java.lang.*;
+
 
 class Solution {
-    int shortest = Integer.MAX_VALUE;
+//     <입력>
+//         1이상, 1000이하 길이의 문자열
+//     <출력>
+//         가장 짧은 압축된 문자열의 길이
+    
+//     1. 1부터 1000이하의 토큰 길이로 문자열 압축을 시도한다.
+//     - 같은 문자열이 연속되는 경우에만 숫자를 센다. (다른 문자열을 사이에 두고 반복되면 따로 셈)
+//     - 1개만 있으면 숫자를 생략한다.
+//     2. 해당 토큰 길이가 가장 짧을 때 변수를 업데이트한다.
+//     3. 토큰 길이를 반환한다.
     public int solution(String s) {
-        recursion(s, 1);
-        return shortest;
+        int answer = getAnswer(s);
+        return answer;
     }
     
-    public void recursion(String input, int N) {
-        if(N == input.length()) {
-            shortest = shortest > N ? N : shortest;
-            return;
+    public int getAnswer(String s) {
+        int lengthLimit = s.length();
+        int min = lengthLimit == 1 ? 1 : Integer.MAX_VALUE;
+        for(int i = 1 ; i < lengthLimit ; i++) {
+            int currentLength = zipString(s, i);
+            min = Math.min(min, currentLength);
         }
         
-        String result = zip(split(input, N));
-        shortest = shortest > result.length() ? result.length() : shortest;
-        recursion(input, N + 1);
+        return min;
     }
     
-    public Queue<String> split(String input, int N) {
-        Queue<String> tokenQueue = new LinkedList();
-        while(input.length() > N) {
-            String token = input.substring(0, N);
-            tokenQueue.offer(token);
-            input = input.substring(N);
-        }
-        
-        tokenQueue.offer(input);
-        return tokenQueue;
-    }
-    
-    public String zip(Queue<String> tokenQueue) {
-        String prevToken = tokenQueue.poll();
-        String currentToken = "";
-        int count = 1;
+    public int zipString(String target, int tokenSize) {
         StringBuilder sb = new StringBuilder();
-        while(!tokenQueue.isEmpty()) {
-            currentToken = tokenQueue.poll();
+        int count = 1;
+        String prevToken = target.substring(0, tokenSize);
+        for(int i = tokenSize ; i < target.length() ; i += tokenSize) {
+            if(tokenSize + i > target.length()) {
+                String currentToken = target.substring(i);
+                if(prevToken.equals(currentToken)) {
+                    count++;
+                } else {
+                    if(count > 1) {
+                        sb.append(count);
+                    }
+                    sb.append(prevToken);
+                    count = 1;
+                    prevToken = currentToken;
+                }
+                break;
+            }
+            
+            String currentToken = target.substring(i, tokenSize + i);
+            
             if(prevToken.equals(currentToken)) {
                 count++;
-            } else if(!prevToken.equals(currentToken)){
-                if(count == 1) {
-                    sb.append(prevToken);
-                } else {
-                    sb.append(count).append(prevToken);
-                }
-                count = 1;
-                prevToken = currentToken;
-            }
-        }
-        if(tokenQueue.isEmpty()) {
-            if(count == 1) {
-                sb.append(prevToken);
             } else {
-                sb.append(count).append(prevToken);
+                if(count > 1) {
+                    sb.append(count);
+                }
+                sb.append(prevToken);
+                count = 1;
             }
-            count = 1;
             prevToken = currentToken;
         }
         
-        return sb.toString();
+        if(count > 1) {
+            sb.append(count);
+        }
+        sb.append(prevToken);
+                
+        return sb.toString().length();
     }
 }
