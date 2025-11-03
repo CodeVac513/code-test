bcd91c61c8d6dbdac23a764b9115cb1269d0fb5e
Park-Mu-Seong <63047990+CodeVac513@users.noreply.github.com>
2025-11-03 13:16:50 +0900

    [Gold IV] Title: 여행 가자, Time: 188 ms, Memory: 19232 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 import java.lang.*;
@@ -9,7 +10,7 @@ public class Main {
     // 그래서 E C B C D를 입력할 때, 모두 같은 대표 노드를 가리킨다면 서로 이어져 있음을 알 수 있음.
     // 근데 N이 작아서 플로이드 워셜로도 풀 수 있을 것 같은데? 두 가지 다 도전해보자.
 
-    static int[][] distance;
+    static int[] parent;
     static int n;
     static int m;
 
@@ -19,64 +20,63 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
 
-        final int INF = 1000 * 200 + 1;
-
-        distance = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(distance[i], INF);
-            distance[i][i] = 0;
+        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            int iter = st.countTokens();
+            for (int j = 1; j <= iter; j++) {
                 int status = Integer.parseInt(st.nextToken());
                 if (status == 1) {
-                    distance[i][j] = 1;
-                    distance[j][i] = 1;
+                    union(i, j);
                 }
             }
         }
-
-        dp();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int current = Integer.parseInt(st.nextToken()) - 1;
-        int iter = st.countTokens();
-        boolean flag = true;
-        for (int i = 0; i < iter; i++) {
-            int next = Integer.parseInt(st.nextToken()) - 1;
 
-            if (distance[current][next] < INF) {
-                current = next;
-            } else {
-                flag = false;
+
+        int start = Integer.parseInt(st.nextToken());
+        int root = find(start);
+        int iter = st.countTokens();
+        boolean isPossible = true;
+        for (int i = 0; i < iter; i++) {
+            int currentNode = Integer.parseInt(st.nextToken());
+            int currentRoot = find(currentNode);
+
+            if (currentRoot != root) {
+                isPossible = false;
                 break;
             }
         }
 
-        if (flag) {
+        if (isPossible) {
             bw.write("YES");
         } else {
             bw.write("NO");
         }
+
 
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static void dp() {
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (distance[i][j] > distance[i][k] + distance[k][j]) {
-                        distance[i][j] = distance[i][k] + distance[k][j];
-                    }
-                }
-            }
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            parent[b] = a;
         }
     }
 
+    static int find(int a) {
+        if (parent[a] == a)
+            return a;
+
+        return parent[a] = find(parent[a]);
     }
+}
