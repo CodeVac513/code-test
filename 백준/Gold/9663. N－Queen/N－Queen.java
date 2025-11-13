5711bc0b688ceda24d96b5e954bac2bc340228a1
Park-Mu-Seong <63047990+CodeVac513@users.noreply.github.com>
2025-11-13 20:41:32 +0900

    [Gold IV] Title: N-Queen, Time: 5880 ms, Memory: 14760 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,63 +1,64 @@
-
-
 import java.io.*;
 import java.util.*;
 import java.lang.*;
 
 public class Main {
-    // 9663번
+    // 9663
     // 백트래킹
-    // 어떤 상태를 복원해야 하는가?
-    // 한 행에서 열을 한 칸 씩 옮기며 모든 경우의 수를 탐색한다.
-    // 그 다음 지금까지 놓인 queen들과 충돌하는 경우가 있는지 isPossible 따위를 호출해서 확인한다.
-    // 통과한다면 재귀함수의 입력인 row를 하나 더해서 호출한다.
-    // isPossible은 같은 열에 있는지만 검색한다. (row는 이미 하나씩 놓으면서 검사 중임.)
+    // 유명한 백트래킹 문제
+    // 행마다 특정 열에 queen을 배치함
+    // 다음 행으로 넘어갈 때마다 배치될 수 있는 queen 인지 검사함.
+    // 대각선은 기울기의 ABS가 1인지만 확인하면 됨.
+    // row에서 하나의 col을 골라야 함.
+    // queensCol[row] = col → "row번째 행에 col번 열에 퀸을 놓는다"
+    // 그러면 k번째 row에서 l번째 col에 위치한다고 가정하면 |row - k| == |col - l|이 되면 기울기가 1인 거고 위치할 수 없음.
 
+    static int[] queensCol;
     static int N;
-    static int[] queen;
     static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        queen = new int[N];
         answer = 0;
-
-        backtrack(0);
+        queensCol = new int[N + 1];
+        backtracking(0);
 
         bw.write(String.valueOf(answer));
-
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static void backtrack(int row) {
-
+    static void backtracking(int row) {
         if (row == N) {
             answer++;
             return;
         }
 
         for (int col = 0; col < N; col++) {
-            queen[row] = col;
-            if (isPossible(row)) {
-                backtrack(row + 1);
-            }
-        }
-    }
+            queensCol[row] = col;
 
-    static boolean isPossible(int row) {
-        for (int i = 0; i < row; i++) {
-            if (queen[i] == queen[row]) {
-                return false;
+            boolean canLocated = true;
+            for (int i = 0; i < row; i++) {
+                if (queensCol[i] == col) {
+                    canLocated = false;
+                    break;
+                }
+
+                if (Math.abs(row - i) == Math.abs(queensCol[row] - queensCol[i])) {
+                    canLocated = false;
+                    break;
+                }
             }
-            if (Math.abs(row - i) == Math.abs(queen[row] - queen[i])) {
-                // 기울기가 1인지 검사해서 대각선에 위치한 퀸이 있는지 확인
-                return false;
-            }
+
+            if (!canLocated)
+                continue;
+
+            backtracking(row + 1);
         }
-        return true;
     }
 }
+
+
